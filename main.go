ec168b3ff6ea91b6d062b675cdb729c00ef28e8b
Gianluca Recchia <g.recchia@desotech.it>
2021-07-29 16:46:56 +0200

    Add ability to run the provider in debug mode

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/techBeck03/terraform-provider-guacamole/guacamole"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "Run the provider in debug mode")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
 			return guacamole.Provider()
 		},
-	})
+	}
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/techBeck03/guacamole", opts)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+
+	plugin.Serve(opts)
 }
